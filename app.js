b09789c6bb3d202e340d5ffad72a0b7c379054ba
Gongqi Lin <Joseph@LinGongqis-MBP.gateway>
2015-06-30 18:31:41 +1000

    v1.0

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var mongoose = require('mongoose');
+var passport = require('passport');
 
 // connect MongoDB
 mongoose.connect('mongodb://localhost/news', function(err,db){
@@ -20,9 +21,12 @@ mongoose.connect('mongodb://localhost/news', function(err,db){
 
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,6 +39,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ var router = express.Router();
 
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+var passport = require('passport');
+
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+
 
 
 
@@ -23,7 +29,7 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
 
   post.save(function(err, post){
@@ -63,7 +69,7 @@ router.get('/posts/:post', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
@@ -71,7 +77,7 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/downvote', function(req, res, next) {
+router.put('/posts/:post/downvote', auth, function(req, res, next) {
   req.post.downvote(function(err, post){
     if (err) { return next(err); }
 
@@ -79,7 +85,7 @@ router.put('/posts/:post/downvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
 
@@ -95,7 +101,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment){
     if (err) { return next(err); }
 
@@ -103,12 +109,46 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/downvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/downvote', auth, function(req, res, next) {
   req.comment.downvote(function(err, comment){
     if (err) { return next(err); }
 
     res.json(comment);
   });
 });
+
+router.post('/register', function(req, res, next){
+	  if(!req.body.username || !req.body.password){
+	    return res.status(400).json({message: 'Please fill out all fields'});
+	  }
+
+	  var user = new User();
+
+	  user.username = req.body.username;
+
+	  user.setPassword(req.body.password)
+
+	  user.save(function (err){
+	    if(err){ return next(err); }
+
+	    return res.json({token: user.generateJWT()})
+	  });
+	});
+
+router.post('/login', function(req, res, next){
+	  if(!req.body.username || !req.body.password){
+	    return res.status(400).json({message: 'Please fill out all fields'});
+	  }
+
+	  passport.authenticate('local', function(err, user, info){
+	    if(err){ return next(err); }
+
+	    if(user){
+	      return res.json({token: user.generateJWT()});
+	    } else {
+	      return res.status(401).json(info);
+	    }
+	  })(req, res, next);
+	});
 
 module.exports = router;
